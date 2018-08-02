ba3706d323cadbdc8643574a40711d29466fa304
Daniel Wolkow <volkov12@rambler.ru>
2018-08-03 01:36:25 +0300

    [update] -- checker

--- checker.sh
+++ checker.sh
@@ -3,5 +3,8 @@
 FILE=$1
 ret=$(iconv -t UTF-8 ${FILE} -o ${FILE}-conv)
 if [ $? -ne 0 ]; then
+        echo "Length ${FILE}-conv:"
         cat ${FILE}-conv | wc -l
+        echo "Length ${FILE}"
+        cat ${FILE} | wc -l
 fi
